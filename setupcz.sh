b00efaf31ee03aa8c94548f51c9924e2ecc23acd
CharmingHue <1299670883@qq.com>
2024-01-23 21:23:34 +0800

    feat(setupcz.sh): add setupcz.sh for installing commitizen

--- setupcz.sh
+++ setupcz.sh
@@ -2,13 +2,13 @@ mkdir /root/n
 cd /root/n
 apt-get update
 apt-get -y install npm
-npm install -g n
-n 20.5.0
-npm install -g npm@10.3.0
 npm config set registry https://registry.npm.taobao.org
 # npm config set registry http://registry.npmmirror.com
+npm install -g n
+n 20.5.0
+hash -r
+npm install -g npm@10.3.0
 npm install -g commitizen
 npm install -g conventional-changelog
-
-commitizen init cz-conventional-changelog --save-dev --save-exact
 cd -
+commitizen init cz-conventional-changelog --save-dev --save-exact
