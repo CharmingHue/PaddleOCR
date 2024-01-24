9a0ef4c6873fedd5e0e74cde25ce2f2276bdd33c
CharmingHue <1299670883@qq.com>
2024-01-24 15:35:48 +0800

    feat(train data): add get_dataset.sh File for getting train dataset

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir train_data && cd train_data
-wget -O train_val_test_lmdb.zip "https://charminghue.obs.cn-north-4.myhuaweicloud.com:443/LMDB/train_val_test_lmdb.zip?AccessKeyId=PHZJJHINZ3VQJ0RBZ8XI&Expires=1706167028&Signature=sNawzqTnwd%2B5VT1HyoVQsQNmg/M%3D"
+wget -O train_val_test_lmdb.zip "https://charminghue.obs.cn-north-4.myhuaweicloud.com:443/LMDB/train_val_test_lmdb.zip?AccessKeyId=PHZJJHINZ3VQJ0RBZ8XI&Expires=1708673676&Signature=ouAtGXw7xtSwzBvBIgKaL0lC%2BA4%3D"
 unzip train_val_test_lmdb.zip
 sh extract-datasets.sh
 rm test.zip train.zip val.zip
