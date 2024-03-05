628e9931a2a913a3d64977dde6af6343babd54e6
CharmingHue <1299670883@qq.com>
2024-03-05 11:11:10 +0800

    feat(eval_all):add eval all for rec model to tools

---
+++ tools/eval_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+config_path=${1}
+model_path=${2}
+
+# 定义数据集名称数组
+datasets=('IC13_857' 'SVT' 'IIIT5k' 'IC15_1811' 'SVTP' 'CUTE80')
+
+# 循环遍历数据集数组
+for dataset in "${datasets[@]}"
+do
+
+    echo "Evaluating dataset ${dataset}..."
+    python tools/eval.py -c $config_path -o Global.pretrained_model=$model_path Eval.dataset.data_dir=train_data/test/${dataset}
+    # bash ./tools/eval_all.sh ./configs/rec/rec_cloformer_cppd.yml ./output/rec/cloformer_cppd_base/iter_epoch_6.pdparams
+done
+
+echo "Evaluation completed."
+
